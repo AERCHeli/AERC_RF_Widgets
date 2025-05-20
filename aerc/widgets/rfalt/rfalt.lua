578dffa7c53d8f3c2b20e34a40052429138d147d
AE RC <andyercheli@gmail.com>
2025-05-20 10:22:54 +1000

    Update rfalt.lua
    Final updates before v1.0 release.

--- aerc/widgets/rfalt/rfalt.lua
+++ aerc/widgets/rfalt/rfalt.lua
@@ -13,10 +13,10 @@
 -- #                                                                       #
 -- #########################################################################
 
--- RF Widgets - Rotorflight & ETHOS
+-- AERC Widgets - Rotorflight & ETHOS
 -- Author: Andy.E (Discord: AJ#9381)
 -- Version: 1.0
--- Widget Name: RF Altitude
+-- Widget Name: AERC Altitude
 
 -- === Global Settings ===
 local GRACE_DELAY = 15 -- Time (seconds) to blink before showing real Altitude after telemetry connected
